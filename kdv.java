3b64e244444fad64188435ad5a2ddf964f364c80
Alper Karabıyık <120407040+karabiyikk@users.noreply.github.com>
2022-12-13 02:39:18 +0300

    Update kdv.java

--- kdv.java
+++ kdv.java
@@ -1,17 +1,19 @@
 import java.util.Scanner;
 public class kdv {
-    public static void main(String[] args) {
-        double tutar, kdv, kdvli, kdvOran = 0.18;
+        public static void main(String[] args) {
+        double tutar, kdv, kdvli, kdvOran = 0.18, kdvOran2 = 0.08;
         Scanner para = new Scanner(System.in);
 
         System.out.print("KDV hesaplanacak tutarı girin: ");
         tutar = para.nextDouble();
-        System.out.println("KDV'siz tutar : "+ tutar);
 
-        kdv = tutar * kdvOran;
-        System.out.println("KDV tutarı : " + kdv);
+        System.out.println("KDV'siz tutar : " + tutar);
 
-        kdvli = tutar + kdv;
+        kdv = tutar < 1000 ? kdvOran : kdvOran2;
+        System.out.println("KDV oranı : " + kdv);
+        System.out.println("KDV miktarı : " + (tutar * kdv));
+
+        kdvli = tutar +(tutar * kdv);
         System.out.print("KDV'li tutar : " + kdvli);
     }
 }
